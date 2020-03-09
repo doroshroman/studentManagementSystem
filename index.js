688ab4ff7b7e87661aa96aeead3725a5d524d2ec
Roman <doroshroman27@gmail.com>
2020-03-09 14:13:39 +0200

    created server

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send("Hello World!");
-});
-
 app.listen(3000, function () {
     console.log('App listening on port 3000!');
 });
+app.use(express.static('public'));
+app.use(express.json({ limit: '1mb' }));
+
+app.get('/api', (req, res) => {
+    res.json({'test': 10});
+});
+
+
